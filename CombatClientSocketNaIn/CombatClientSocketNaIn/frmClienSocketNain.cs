bc680e351be5d2c2b37c6e9a8c83d0256d08457a
jflevis <89398420+jflevis@users.noreply.github.com>
2021-11-16 11:36:34 -0500

    debut projet combat nains elfes

---
+++ CombatClientSocketNaIn/CombatClientSocketNaIn/frmClienSocketNain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CombatClientSocketNaIn.Classes;
+
+
+namespace CombatClientSocketNaIn
+{
+    public partial class frmClienSocketNain : Form
+    {
+        Random m_r;
+        Elfe m_elfe;
+        Nain m_nain;
+        public frmClienSocketNain()
+        {
+            InitializeComponent();
+            m_r = new Random();
+
+            btnReset.Enabled = false;
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        void Reset()
+        {
+            m_nain = new Nain(m_r.Next(10, 20), m_r.Next(2, 6), m_r.Next(0, 3));
+            picNain.Image = m_nain.Avatar;
+            lblVieNain.Text = "Vie: " + m_nain.Vie.ToString(); ;
+            lblForceNain.Text = "Force: " + m_nain.Force.ToString();
+            lblArmeNain.Text = "Arme: " + m_nain.Arme;
+
+            m_elfe = new Elfe(1, 0, 0);
+            picElfe.Image = m_elfe.Avatar;
+            lblVieElfe.Text = "Vie: " + m_elfe.Vie.ToString();
+            lblForceElfe.Text = "Force: " + m_elfe.Force.ToString();
+            lblSortElfe.Text = "Sort: " + m_elfe.Sort.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btnFrappe.Enabled = true;
+            Reset();
+        }
+
+        private void btnFrappe_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ CombatServeurSocketElfe/CombatServeurSocketElfe/frmServeurSocketElfe.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CombatServeurSocketElfe.Classes;
+
+namespace CombatServeurSocketElfe
+{
+    public partial class frmServeurSocketElfe : Form
+    {
+        Random m_r;
+        Nain m_nain;
+        Elfe m_elfe;
+        TcpListener m_ServerListener;
+        Socket m_client;
+        Thread m_thCombat;
+
+        public frmServeurSocketElfe()
+        {
+            InitializeComponent();
+            m_r = new Random();
+            
+            btnReset.Enabled = false;
+            //Démarre un serveur de socket (TcpListener)
+            
+            lstReception.Items.Add("Serveur démarré !");
+            lstReception.Items.Add("PRESSER : << attendre un client >>");
+            lstReception.Update();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        void Reset()
+        {
+            m_nain = new Nain(1, 0, 0);
+            picNain.Image = m_nain.Avatar;
+            AfficheStatNain();
+
+            m_elfe = new Elfe(m_r.Next(10, 20), m_r.Next(2, 6), m_r.Next(2, 6));
+            picElfe.Image = m_elfe.Avatar;
+            AfficheStatElfe();
+ 
+            lstReception.Items.Clear();
+        }
+
+        void AfficheStatNain()
+        {
+
+
+            
+            this.Update(); // pour s'assurer de l'affichage via le thread
+        }
+        void AfficheStatElfe()
+        {
+            
+
+
+            this.Update(); // pour s'assurer de l'affichage via le thread
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            Reset();
+        }     
+
+        private void btnAttente_Click(object sender, EventArgs e)
+        {
+            // Combat par un thread
+            
+        }
+        public void Combat() 
+        {
+            // déclarations de variables locales 
+
+            try
+            {
+                // tous le code de traitement
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void btnFermer_Click(object sender, EventArgs e)
+        {
+            // il faut avoir un objet elfe et un objet nain instanciés
+            //m_elfe.Vie = 0;
+            //m_nain.Vie = 0;
+            try
+            {
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void frmServeurSocketElfe_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btnFermer_Click(sender,e);
+            try
+            {
+                // il faut avoir un objet TCPListener existant
+                // m_ServerListener.Stop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+    }
+}
